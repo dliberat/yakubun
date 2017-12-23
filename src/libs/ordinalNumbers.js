c3e5023d5b31b881b0e9f3ca5cc908f049166c01
Dan <garroad_ran@hotmail.com>
2017-12-23 11:01:14 +0000

    Add ordinal numbers module

--- src/libs/ordinalNumbers.js
+++ src/libs/ordinalNumbers.js
@@ -1,70 +1,78 @@
-/* eslint-disable */
 /*
 Check ordinal numbers to ensure that they have the proper trailing letters
 */
 
+import indicator from 'ordinal/indicator';
 import * as general from './lib-generaluse';
 
-const oRules = {
-  en: {
-    1: 'st',
-    2: 'nd',
-    3: 'rd',
-    general: 'th',
-  },
-};
+let accumulator;
 
-function ordinalNumbers(source, target, checkOptions = {}, oAccumulator) {
-  // set EN as the default language
-  if (!checkOptions.targetLang) { checkOptions.targetLang = 'en'; }
-
-  // abort if there are no rules for the targetLang
-  if (oRules[checkOptions.targetLang]) {
-    var rules = oRules[checkOptions.targetLang];
-  } else {
-    return [null, oAccumulator];
-  }
-
-  return runRegex(target, rules, oAccumulator);
+function returnString(arr) {
+  const retval = `Check ordinal numbers: ${arr.join(', ')}`;
+  return [retval, accumulator];
 }
 
-function runRegex(target, rules, oAccumulator) {
+function returnNull() {
+  return [null, accumulator];
+}
+
+function hasBadSuffix(arr) {
+  // don't flag AM and PM
+  if (arr[2].toLowerCase() === 'am' || arr[2].toLowerCase() === 'pm') {
+    return false;
+  }
+
+  const correctSuffix = indicator(arr[1]);
+  if (correctSuffix !== arr[2].trim()) {
+    return true;
+  }
+
+  return false;
+}
+
+function compareWithRules(arr) {
+  const suspiciousNumbers = [];
+
+  arr.forEach((subArr) => {
+    if (hasBadSuffix(subArr)) {
+      suspiciousNumbers.push(subArr[0].trim());
+    }
+  });
+
+  if (suspiciousNumbers.length > 0) {
+    return returnString(suspiciousNumbers);
+  }
+
+  return returnNull();
+}
+
+function runRegex(target) {
   // regex to search for ordinal numbers
-  const re = new RegExp('([0-9]+)([a-z][a-z]?)(?:\\W|$)', 'gi');
+  const re = new RegExp('([0-9]+)([a-z][a-z])(?:\\W|$)', 'gi');
 
   const arr = general.regexReturnAllMatches(target, re);
-  /* arr contains
+  /* each arr contains
     [0]: full match (1st, 2nd, etc.);
     [1]: numeral (1, 2, etc.)
     [2]: suffix (st, nd, etc.)
     */
-  return compareWithRules(arr, rules, oAccumulator);
-}
 
-function compareWithRules(arr, rules, oAccumulator) {
-  // assume no errors
-  let flag = false;
-
-  // loop through the results, check that they match the rules
-  for (let i = 0; i < arr.length; i++) {
-    const strNum = arr[i][1].toString();
-    const num = strNum.substr(strNum.length - 1);
-    // if there's a specific rule for that number
-    if (rules[num]) {
-      if (arr[i][2] != rules[num]) { flag = true; }
-    } else {
-      // if there's no specific rule, use the general rule
-      if (arr[i][2] != rules.general) { flag = true; }
-    }
+  if (arr.length > 0) {
+    return compareWithRules(arr);
   }
 
-  return output(flag, oAccumulator);
+  return returnNull();
 }
 
-function output(flag, oAccumulator) {
-  const retval = flag ? 'Check ordinal numbers.' : null;
+function ordinalNumbers(source, target, checkOptions = {}, oAccumulator) {
+  accumulator = oAccumulator;
 
-  return [retval, oAccumulator];
+  if (checkOptions.targetLang !== 'en') {
+    general.metalogger('Ordinal number checks are only supported for English.');
+    return returnNull();
+  }
+
+  return runRegex(target);
 }
 
 export default ordinalNumbers;
--- test/ordinalNumbers.js
+++ test/ordinalNumbers.js
@@ -1,41 +1,49 @@
-/*const expect = require('chai').expect;
+const expect = require('chai').expect;
 import ordinalNumbers from '../src/libs/ordinalNumbers.js';
+
+const options = {
+    targetLang: 'en',
+}
 
 describe('Catch incorrect ordinal numbers', function(){
     it('Return null for 1st, 2nd, 3rd, 4th, 21st', function(){
        var target = '1st, 2nd, 3rd, 4th, 21st';
-       var res = ordinalNumbers('', target);
+       var res = ordinalNumbers('', target, options);
        expect(res[0]).to.equal(null);
     });
     it('Return null for 5tastic 5th Anniversary', function(){
         var target = '5tastic 5th Anniversary';
-        var res = ordinalNumbers('', target);
+        var res = ordinalNumbers('', target, options);
         expect(res[0]).to.equal(null);
     });
     it('Flag 1nd', function(){
        var target = '1nd';
-       var res = ordinalNumbers('', target);
-       expect(res[0]).to.equal('Check ordinal numbers.');
+       var res = ordinalNumbers('', target, options);
+       expect(res[0]).to.equal('Check ordinal numbers: 1nd');
     });
     it('Flag 2rd', function(){
         var target = '2rd';
-        var res = ordinalNumbers('', target);
-        expect(res[0]).to.equal('Check ordinal numbers.');
+        var res = ordinalNumbers('', target, options);
+        expect(res[0]).to.equal('Check ordinal numbers: 2rd');
     });
     it('Flag 101th', function(){
         var target = '101th';
-        var res = ordinalNumbers('', target);
-        expect(res[0]).to.equal('Check ordinal numbers.');
+        var res = ordinalNumbers('', target, options);
+        expect(res[0]).to.equal('Check ordinal numbers: 101th');
     });
     it('Don\'t flag am pm', function(){
        var target = 'Until 11am, Dec 27 (GMT+9)';
-       var res = ordinalNumbers('', target);
+       var res = ordinalNumbers('', target, options);
        expect(res[0]).to.equal(null);
     });
     it('Don\'t flag multiplication x', function(){
        var target = '2x the decorations';
-       var res = ordinalNumbers('', target);
+       var res = ordinalNumbers('', target, options);
        expect(res[0]).to.equal(null);
     });
-})
-*/
+    it('Don\'t flag two numbers multiplied without a space', function(){
+       const target = 'Get 4x5 stars!';
+       const res = ordinalNumbers('', target, options);
+       expect(res[0]).to.equal(null);
+    });
+});
