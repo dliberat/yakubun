514636e1d066a787bc138b699307d0e2f944e590
Dan Liberatori <garroad_ran@hotmail.com>
2018-05-29 08:39:35 +0900

    Add integration testing

--- test-integration/entrypoint.test.js
+++
@@ -1,9 +0,0 @@
-/* global describe, it */
-const { expect } = require('chai');
-const yakubun = require('../dist/yakubun');
-
-describe('Integration tests', () => {
-  it('Module exports the "scan" method', () => {
-    expect(yakubun.scan).to.be.a('function');
-  });
-});
